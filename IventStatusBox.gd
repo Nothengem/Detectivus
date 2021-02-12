c3d156fa02b9e791b67edeaa30c24e4caced5571
Nothengem <nothengem43656@gmail.com>
2021-02-12 18:00:52 +0300

    Начал новую фичу

---
+++ IventStatusBox.gd
@@ -0,0 +1,13 @@
+extends CenterContainer
+
+var TeamUp
+var CustomersUp
+var CentralBankUp
+var MoneyUp
+var TeamDown
+var CustomersDown
+var CentralBankDown
+var ModeyDown
+
+func IventStatus1_start():
+	pass
